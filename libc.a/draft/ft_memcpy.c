5de488f9b8834e5536333b701be794c25a18f95f
Sakonkiat Rungsaranon <srungsar@z2t7c3.42bangkok.com>
2023-10-28 16:31:40 +0700

    split XD and T_T memcpy

--- libc.a/draft/ft_memcpy.c
+++ libc.a/draft/ft_memcpy.c
@@ -7,12 +7,18 @@ char	*ft_memcpy(void *dest, const void *src, size_t amount)
 	char *start;
 
 	start = dest;
-	while (amount)
+	// if (!(*(char *)dest) && !(*(char *)src))
+	// 	return start;
+	while (amount--)
 	{
 		*(char *)dest = *(char *)src;
-		amount--;
 		dest++;
 		src++;
 	}
 	return (start);
 }
+
+// int	main()
+// {
+// 	printf("%s", ft_memcpy(((void *)0), ((void *)0), 3));
+// }
